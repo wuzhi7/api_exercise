198859110e7e5d17ca56f261e30453a86c8ea637
liyangbin <liyanbin98@gmail.com>
2017-08-15 19:04:36 +0800

    finish step2

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
     end
   end
 
+namespace :api, :defaults => { :format => :json } do
+  namespace :v1 do
+    get "/trains" => "trains#index", :as => :trains
+    get "/trains/:train_number" => "trains#show", :as => :train
+
+    get "/reservations/:booking_code" => "reservations#show", :as => :reservation
+    post "/reservations" => "reservations#create", :as => :create_reservations
+    patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
+    delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+
+  end
+end
+
+
 end
